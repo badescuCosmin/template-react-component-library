78971463fd823fa34be6b45fbd9e4792cd4b096a
Badescu Petre Cosmin <cosmin.badescu@tevvo.com>
2022-06-17 11:36:30 +0300

    clear package

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
 export { Checkbox } from "./checkbox";
 export { ThemeProvider } from "./theme";
 export { Switch } from "./switch";
-export { typography } from "./theme/tokens/typography";
-export { Button } from "./button";
+export { Button as MuiBtn } from "./button";
 export { WithLabelDateInput, WithLabelInput } from "./withLabelInput";
